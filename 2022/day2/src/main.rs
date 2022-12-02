d7c52b1789e997974d9bb861e9005e8f6f21b459
danieljohannsson <daniel.johannsson1999@gmail.com>
2022-12-02 14:31:10 +0100

    first star completed

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -1,3 +1,35 @@
+use std::fs;
+//Rock: A X 1
+//Paper: B Y 2
+//Scissor: C Z 3
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").unwrap();
+    let mut lines = contents.lines();
+    let mut total = 0;
+    for line in lines {
+        let mut c = line.chars();
+        let expected = c.next().unwrap();
+        c.next();
+        let response = c.next().unwrap();
+        c.next();
+        match expected {
+            'A'=> {match response {
+                'X'=> total += 4,
+                'Y'=> total += 8,
+                'Z'=> total += 3,
+                _ => println!("")}},
+            'B'=> {match response {
+                'X'=> total += 1,
+                'Y'=> total += 5,
+                'Z'=> total += 9,
+                _ => println!("")}},
+            'C'=> {match response {
+                'X'=> total += 7,
+                'Y'=> total += 2,
+                'Z'=> total += 6,
+                _ => println!("")}},
+            _ => println!("")
+        }
+    }
+    println!("total: {}", total);
 }
