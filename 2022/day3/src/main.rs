1ee5379f00ddfd19a92246b19d03e1f100538db6
danieljohannsson <daniel.johannsson1999@gmail.com>
2022-12-05 10:14:12 +0100

    day3 completed

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -5,25 +5,29 @@ fn main() {
     let lines = contents.lines();
     let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
     let mut total = 0;
-    for a in alphabet.iter() {
-        println!("{}", a)
-    }
+    let mut block: Vec<&str> = Vec::new();
     for line in lines {
-        let mut element = '0';
-        let (first, last) = line.split_at(line.len()/2);
-        let ch1: Vec<char> = last.chars().collect();
-        let ch2: Vec<char> = first.chars().collect();
-        for n in 0..first.len() {
-            for u in 0..first.len() {
-                if ch1[n] == ch2[u] {
-                    element = ch1[n];
-                }
-            }   
+        block.push(line);
+        if block.len() == 3 {
+            let mut element = '0';
+            let ch1: Vec<char> = block[0].chars().collect();
+            let ch2: Vec<char> = block[1].chars().collect();
+            let ch3: Vec<char> = block[2].chars().collect();
+            for n in 0..ch1.len() {
+                for u in 0..ch2.len() {
+                    for i in 0..ch3.len() {
+                        if ch1[n] == ch2[u] && ch1[n] == ch3[i] && ch2[u] == ch3[i] {
+                            element = ch1[n];
+                        }
+                    }
+                }   
+            }
+            total += alphabet.iter().position(|&s| s == element).unwrap_or_default() + 1;
+            block = Vec::new();
+            println!("{}",alphabet.iter().position(|&s| s == element).unwrap_or_default());
+            println!("{}", element);
+            println!("{}", line);
         }
-        total += alphabet.iter().position(|&s| s == element).unwrap_or_default() + 1;
-        // println!("{}",alphabet.iter().position(|&s| s == element).unwrap_or_default());
-        // println!("{}", element);
-        // println!("{}", line);
     }
     println!("{}", total);
 
